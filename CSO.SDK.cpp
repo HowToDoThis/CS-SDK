f50dd46deed7b91e53de63a9115d1e383b871743
Xein <nopeople123456789@gmail.com>
2024-10-07 03:56:45 +0800

    VS support

---
+++ CSO.SDK.cpp
@@ -0,0 +1,9 @@
+// CSO.SDK.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+
+// TODO: This is an example of a library function
+void fnCSOSDK()
+{
+}
---
+++ pch.h
@@ -0,0 +1,16 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <Windows.h>
+
+#include "include/defines.h"
+#include "include/vector.h"
+#include "include/weapon.h"
+
+#include "include/edict.h"
+
+#include "include/temp/mp/everything.h"
+
+#include "include/temp/client/everything.h"
+
+#endif //PCH_H
